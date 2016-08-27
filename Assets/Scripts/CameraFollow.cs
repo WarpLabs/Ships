a6c8b76fbc6f8bb0fd272d6185a49fedf898b24d
WilliamQin675 <william.qin51@gmail.com>
2016-08-27 19:42:20 -0400

    Add Player - Movement + Camera Follow + Mouse Follow + Collider

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject TargetObject;
+
+	private Vector3 Offset;
+
+	void Start () {
+
+		Offset = transform.position - TargetObject.transform.position;
+
+	}
+
+	void LateUpdate () {
+
+		transform.position = TargetObject.transform.position + Offset;
+
+	}
+
+}
