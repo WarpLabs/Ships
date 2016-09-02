3e0e23c1c30790d062d0b7d3f164c35ef03a56a0
WilliamQin675 <william.qin51@gmail.com>
2016-09-01 23:59:18 -0400

    The Revert

--- Assets/Scripts/Ship Scripts/HealthControl.cs
+++ Assets/Scripts/Ship Scripts/HealthControl.cs
@@ -42,10 +42,31 @@ public class HealthControl : MonoBehaviour {
             {       
                 HealthBar.localScale = new Vector3(percentHealth, HealthBar.localScale.y, HealthBar.localScale.z);
                 HealthBar.position = new Vector3(HealthBar.position.x - ((damageDone/totalHealth) / 2), HealthBar.position.y, HealthBar.position.z);
-            }
-                        
+            }                        
             Destroy(other.gameObject);
         }
+
+        if (other.transform.parent.gameObject.CompareTag("Turret"))
+        {
+            bulletDamage = other.transform.parent.gameObject.GetComponent<TurretShooter>().damage;
+            damageDone = bulletDamage - armor;
+            health -= damageDone;
+            percentHealth = health / totalHealth;
+
+            bulletKnockback = other.transform.parent.gameObject.GetComponent<TurretShooter>().knockBack;
+            knockBackDone = (bulletKnockback - weight) / 10f;
+            knockBackDirection = (other.transform.position - other.transform.parent.position).normalized;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(knockBackDone * knockBackDirection);
+
+            if (damageDone > 0)
+            {
+                HealthBar.localScale = new Vector3(percentHealth, HealthBar.localScale.y, HealthBar.localScale.z);
+                HealthBar.position = new Vector3(HealthBar.position.x - ((damageDone / totalHealth) / 2), HealthBar.position.y, HealthBar.position.z);
+            }
+            Destroy(other.gameObject);
+        }
+
+
     }
 
     void Update()
