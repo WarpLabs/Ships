3a9be9a07a48424a00b816983d0d2092e7b8b471
WilliamQin675 <william.qin51@gmail.com>
2016-09-02 00:13:03 -0400

    The Master Revert

--- Assets/Scripts/Ship Scripts/HealthControl.cs
+++ Assets/Scripts/Ship Scripts/HealthControl.cs
@@ -13,6 +13,8 @@ public class HealthControl : MonoBehaviour {
     private float knockBackDone;
     private Vector2 knockBackDirection;
 
+	private Rigidbody2D rb2d;
+
     public float armor;
     private float bulletDamage;
     private float damageDone;
@@ -21,52 +23,38 @@ public class HealthControl : MonoBehaviour {
     void Start()
     {
         totalHealth = health;
+		if (GetComponent<Rigidbody2D>() != null)
+			rb2d = GetComponent<Rigidbody2D> ();
     }
 
 	void OnTriggerEnter2D(Collider2D other)
     {
 
-        if (other.transform.parent.gameObject.CompareTag("Gun"))
+		if (LayerMask.LayerToName(other.gameObject.layer) == "PlayerProjectiles")
         {
-            bulletDamage = other.transform.parent.gameObject.GetComponent<GunShooter>().damage;
+			ProjectileExplosion explo = other.GetComponent<ProjectileExplosion> ();
+
+			bulletDamage = explo.Damage;
             damageDone = bulletDamage - armor;
             health -= damageDone;
             percentHealth = health / totalHealth;
 
-            bulletKnockback = other.transform.parent.gameObject.GetComponent<GunShooter>().knockBack;
-            knockBackDone = (bulletKnockback - weight) / 10f;
-            knockBackDirection = (other.transform.position - other.transform.parent.position).normalized;
-            gameObject.GetComponent<Rigidbody2D>().AddForce(knockBackDone * knockBackDirection);
+			if (rb2d != null) {
+
+				bulletKnockback = explo.Knockback;
+				knockBackDirection = other.transform.up;
+				rb2d.AddForce (knockBackDirection * bulletKnockback);
+
+			}
 
             if (damageDone > 0)
             {       
                 HealthBar.localScale = new Vector3(percentHealth, HealthBar.localScale.y, HealthBar.localScale.z);
                 HealthBar.position = new Vector3(HealthBar.position.x - ((damageDone/totalHealth) / 2), HealthBar.position.y, HealthBar.position.z);
-            }                        
-            Destroy(other.gameObject);
-        }
-
-        if (other.transform.parent.gameObject.CompareTag("Turret"))
-        {
-            bulletDamage = other.transform.parent.gameObject.GetComponent<TurretShooter>().damage;
-            damageDone = bulletDamage - armor;
-            health -= damageDone;
-            percentHealth = health / totalHealth;
-
-            bulletKnockback = other.transform.parent.gameObject.GetComponent<TurretShooter>().knockBack;
-            knockBackDone = (bulletKnockback - weight) / 10f;
-            knockBackDirection = (other.transform.position - other.transform.parent.position).normalized;
-            gameObject.GetComponent<Rigidbody2D>().AddForce(knockBackDone * knockBackDirection);
-
-            if (damageDone > 0)
-            {
-                HealthBar.localScale = new Vector3(percentHealth, HealthBar.localScale.y, HealthBar.localScale.z);
-                HealthBar.position = new Vector3(HealthBar.position.x - ((damageDone / totalHealth) / 2), HealthBar.position.y, HealthBar.position.z);
             }
+                        
             Destroy(other.gameObject);
         }
-
-
     }
 
     void Update()
