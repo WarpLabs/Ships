8367652c19d6f6eb2edac5dd9eabf2cf6401d481
Ihateallbran <Andrew.wang1998@gmail.com>
2016-09-03 11:29:41 -0400

    Line of Sight

--- Assets/Scripts/Ship Scripts/TurretShooter.cs
+++ Assets/Scripts/Ship Scripts/TurretShooter.cs
@@ -1,25 +1,30 @@
+
 using UnityEngine;
 using System.Collections;
 
-public class TurretShooter : MonoBehaviour {
+public class TurretShooter : MonoBehaviour
+{
 
 
-	public GameObject Bullet;
-	public Transform bulletSpawn;
-	private GameObject bullet;
-	public LayerMask enemy;
+    public GameObject Bullet;
+    public Transform bulletSpawn;
+    private GameObject bullet;
+    public LayerMask enemy;
+    public LayerMask ignore;
 
 
-	public float bulletSpeed;
-	public float fireWait;
-	public float damage;
-	public float knockBack;
-	public float stun;
-	public float range;
-	public float lifespan;
+    public float bulletSpeed;
+    public float fireWait;
+    public float damage;
+    public float knockBack;
+    public float stun;
+    public float range;
+    public float lifespan;
 
-	private Collider2D target;
-	private Vector2 direction;
+
+    private bool LOS;
+    private Collider2D target;
+    private Vector2 direction;
 
 
     void Start()
@@ -27,30 +32,38 @@ public class TurretShooter : MonoBehaviour {
         StartCoroutine(Shoot());
     }
 
-	IEnumerator Shoot()
-	{
+    IEnumerator Shoot()
+    {
         while (true)
         {
             if (target != null)
             {
-                bullet = (GameObject)Instantiate(Bullet, bulletSpawn.position, bulletSpawn.rotation);
-                bullet.transform.parent = transform;
-                bullet.GetComponent<Rigidbody2D>().AddForce(direction * bulletSpeed);
 
-                ProjectileExplosion explo = bullet.GetComponent<ProjectileExplosion>();
-                explo.Damage = damage;
-                explo.Knockback = knockBack;
-                explo.Stun = stun;
-                explo.Invoke("Explode", lifespan);
+                if (LOS == true)
+                {
+                    bullet = (GameObject)Instantiate(Bullet, bulletSpawn.position, bulletSpawn.rotation);
+                    bullet.transform.parent = transform;
+                    bullet.GetComponent<Rigidbody2D>().AddForce(direction * bulletSpeed);
 
-                yield return new WaitForSeconds(fireWait);
+                    ProjectileExplosion explo = bullet.GetComponent<ProjectileExplosion>();
+                    explo.Damage = damage;
+                    explo.Knockback = knockBack;
+                    explo.Stun = stun;
+                    explo.Invoke("Explode", lifespan);
+
+                    yield return new WaitForSeconds(fireWait);
+                }
+                else
+                {
+                    yield return null;
+                }
             }
             else
             {
                 yield return null;
             }
         }
-	}
+    }
 
 
     void Update()
@@ -59,48 +72,76 @@ public class TurretShooter : MonoBehaviour {
 
         if (target != null)
         {
-            direction = (target.gameObject.transform.position - transform.position).normalized;
+            LOS = CheckLOS();
+            if (LOS == true)
+            {
+                direction = (target.gameObject.transform.position - transform.position).normalized;
 
-            Vector3 dir = target.gameObject.transform.position - transform.position;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            angle -= 90f;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                Vector3 dir = target.gameObject.transform.position - transform.position;
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                angle -= 90f;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+
         }
     }
 
-	//IEnumerator Start()
-	//{
-	//    while (true)
-	//    {
-	//        target = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y), range, enemy);
+    //IEnumerator Start()
+    //{
+    //    while (true)
+    //    {
+    //        target = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y), range, enemy);
 
 
-	//        if (target != null)
-	//        {
-	//            direction = (target.gameObject.transform.position - transform.position).normalized;
+    //        if (target != null)
+    //        {
+    //            direction = (target.gameObject.transform.position - transform.position).normalized;
 
 
-	//            Vector3 dir = target.gameObject.transform.position - transform.position;
-	//            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-	//            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    //            Vector3 dir = target.gameObject.transform.position - transform.position;
+    //            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+    //            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
 
 
-	//        }
-	//        else
-	//        {
-	//            Debug.Log("nothin");
+    //        }
+    //        else
+    //        {
+    //            Debug.Log("nothin");
 
-	//        }
-	//    }
-	//}
+    //        }
+    //    }
+    //}
 
-	void OnTriggerExit2D(Collider2D other)
-	{
-		if (other.transform.parent == transform)
-		{
-			Destroy(other.gameObject);
-		}
-	}
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.parent == transform)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+
+
+    bool CheckLOS()
+    {
+
+        RaycastHit2D hit;
+        hit = Physics2D.Linecast(transform.position, target.gameObject.transform.position, ignore);
+        Debug.DrawLine(transform.position, target.gameObject.transform.position);
+
+        Debug.Log(1 << hit.collider.gameObject.layer);
+        Debug.Log(enemy.value);
+
+        if (hit.collider != null && (1 << hit.collider.gameObject.layer) == enemy.value)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
 
 }
